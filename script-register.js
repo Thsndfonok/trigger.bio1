01b49cca15fa8c5866be8505b931f4bc80b09651
Thsndfonok <fadmivan@gmail.com>
2025-06-27 00:02:01 +0200

    Update script-register.js

--- script-register.js
+++ script-register.js
@@ -42,7 +42,7 @@ form.addEventListener('submit', async (e) => {
 
   try {
     // Backend POST kérés, endpointot neked kell beállítani
-    const response = await fetch('/api/register', {
+    const response = await fetch('https://trigger.bio/api/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, email, password, customUrl }),
